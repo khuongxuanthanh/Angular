d01b97c8848c5dd74f4945de78e04656e1c2f059
khuongxuanthanh <92875629+khuongxuanthanh@users.noreply.github.com>
2022-01-15 19:34:15 +0700

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { ProductComponent } from './product/product.component';
+import { EditproductComponent } from './editproduct/editproduct.component';
+
+const appRoutes: Routes = [
+  {path:"",component:ProductComponent},
+  {path:"editproduct",component:EditproductComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    EditproductComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
