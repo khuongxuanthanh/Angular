27a63a19317596ecd70095b50a6131841b8e9178
khuongxuanthanh <92875629+khuongxuanthanh@users.noreply.github.com>
2022-01-15 23:06:35 +0700

    23

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,12 +8,16 @@ import { Component } from '@angular/core';
 export class ProductComponent {
   product = [
     {productname:"Product One", productprice:"$22.50"},
-    {productname:"ProductTwo", productprice:"$15.00"},
-    {productname:"ProductThree", productprice:"$190.00"},
-    {productname:"ProductFour", productprice:"$123.82"},
-    {productname:"ProductFive", productprice:"123.75"},
+    {productname:"Product Two", productprice:"$15.00"},
+    {productname:"Product Three", productprice:"$190.00"},
+    {productname:"Product Four", productprice:"$123.82"},
+    {productname:"Product Five", productprice:"123.75"},
   ];
-  productedit = {};
+  pro_edit = {productname:"", productprice:""};
+
   constructor(){}
   ngOnInit(): void{}
+  save(){
+    this.product.push(this.pro_edit);
+  }
 }
