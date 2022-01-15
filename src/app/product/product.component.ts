34ca39107c0a74f3f4b6e953cf38d0582619ad82
khuongxuanthanh <92875629+khuongxuanthanh@users.noreply.github.com>
2022-01-15 20:49:24 +0700

    2

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {}
+export class ProductComponent {
+  product = [
+    {productname:"Product One", productprice:"$22.50"},
+    {productname:"ProductTwo", productprice:"$15.00"},
+    {productname:"ProductThree", productprice:"$190.00"},
+    {productname:"ProductFour", productprice:"$123.82"},
+    {productname:"ProductFive", productprice:"123.75"},
+  ];
+  constructor(){}
+  ngOnInit(): void{}
+}
